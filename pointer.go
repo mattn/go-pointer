c4ef082f1e44cab44838508cb4d6ceebffe1c3fe
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-02 23:23:26 +0900

    first import

---
+++ pointer.go
@@ -0,0 +1,28 @@
+package pointer
+
+import (
+	"unsafe"
+)
+
+var (
+	index uintptr = 0
+	store         = map[uintptr]interface{}{}
+)
+
+func Save(v interface{}) unsafe.Pointer {
+	store[index] = v
+	curr := index
+	index++
+	return unsafe.Pointer(curr)
+}
+
+func Restore(i uintptr) interface{} {
+	if v, ok := store[i]; ok {
+		return v
+	}
+	panic("invalid pointer")
+}
+
+func Unref(i uintptr) {
+	delete(store, index)
+}
